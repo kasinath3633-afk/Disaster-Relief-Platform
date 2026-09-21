2df7b7d71cc601e10fe75bda0fff03cad853aa94
Jishnu Jyothiprakash <191468395+JJP123456789@users.noreply.github.com>
2026-09-21 14:24:31 +0530

    Update requirements.txt

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
 import os
 from dotenv import load_dotenv
 
@@ -14,3 +14,11 @@ SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
     pass
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
